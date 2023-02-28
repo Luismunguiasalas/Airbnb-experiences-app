c762c43ee6775aef98a99da032375eb07fdc142f
Luis Munguia Salas <36023613+Luismunguiasalas@users.noreply.github.com>
2023-02-27 16:42:51 -0800

    Began airbnb project
    have half of the app ready, working on experience cards. Created major components of the app (card, hero, navbar). added styling

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Card () {
+  return (
+    <div className="card">
+      <img src="" alt="" className="card--img"/>
+      <div className="card--stats">
+        <img src="" alt="" className="card--star"/>
+        <span></span>
+        <span className="grey"></span>
+        <span className="grey"></span>
+      </div>
+      <p><span></span></p>
+      <p><span></span></p>
+    </div>
+  )
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar () {
+  return (
+    <nav>
+      <img src="src/assets/airbnb.png" alt="airbnb logo" className="nav--logo"/>
+    </nav>
+  )
+}
+
+export default NavBar
